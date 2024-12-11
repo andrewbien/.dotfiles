631b53e6ab7dc52e7dfd891bd509c9005cc2e06c
Andrew Bien <andrew.bien.bk@gmail.com>
2024-12-11 22:03:29 +0700

    Add aerospace and kitty terminal

--- nix-darwin/.config/nix-darwin/configuration.nix
+++ nix-darwin/.config/nix-darwin/configuration.nix
@@ -1,35 +1,61 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: {
+    # List packages installed in system profile. To search by name, run:
+    # $ nix-env -qaP | grep wget
+    environment.systemPackages =
+    [
+      pkgs.git
+      pkgs.vim
+      pkgs.ctop
+      pkgs.stow
+      pkgs.htop
+      pkgs.inetutils
+      pkgs.toybox
+      pkgs.mkcert
+      pkgs.fd
+      pkgs.fzf
+      pkgs.ffmpeg
+      pkgs.automake117x
+      pkgs.cmake
+      pkgs.entr
+      pkgs.neovim
+      pkgs.tmux
+      pkgs.go
+      pkgs.nodejs_22
+      pkgs.python310
+      pkgs.ripgrep 
+    ];
 
-{
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = [
-    pkgs.vim
-    pkgs.stow
-    pkgs.automake117x
-    pkgs.cmake
-    pkgs.ctop
-    pkgs.entr
-    pkgs.fd
-    pkgs.fzf
-    pkgs.ffmpeg
-    pkgs.go
-  ];
+    homebrew = {
+      enable = true;
+      casks = [
+        "utm"
+        "firefox"
+        "kitty"
+        "aerospace"
+        # "orbstack"
+        # "zalo"
+        #"google-chrome"
+      ];
+      masApps = {
+        "Telegram" = 747648890;
+        "Messenger" = 1480068668;
+      };
+      onActivation.cleanup = "zap";
+    };
 
+    # Necessary for using flakes on this system.
+    nix.settings.experimental-features = "nix-command flakes";
 
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
+    # Enable alternative shell support in nix-darwin.
+    # programs.fish.enable = true;
 
-  # Enable alternative shell support in nix-darwin.
-  # programs.fish.enable = true;
+    # Set Git commit hash for darwin-version.
+    #system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
 
-  # Set Git commit hash for darwin-version.
-  system.configurationRevision = self.rev or self.dirtyRev or null;
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 5;
 
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 5;
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin";
+    # The platform the configuration will be used on.
+    nixpkgs.hostPlatform = "aarch64-darwin";
 }
--- nix-darwin/.config/nix-darwin/flake.nix
+++ nix-darwin/.config/nix-darwin/flake.nix
@@ -1,27 +1,72 @@
 {
   description = "anhbientuan's dotfiles";
 
-  # inputs are other flakes you use within your own flake, dependencies
-  # if you will
   inputs = {
-    # unstable has the 'freshest' packages you will find, even the AUR
-    # doesn't do as good as this, and it's all precompiled.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Homebrew
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+    homebrew-aerospace = {
+      url = "github:nikitabobko/homebrew-aerospace";
+      flake = false;
     };
   };
 
+
   # In this context, outputs are mostly about getting home-manager what it
   # needs since it will be the one using the flake
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeConfigurations = {
-      "anhbientuan" = home-manager.lib.homeManagerConfiguration {
-        # darwin is the macOS kernel and aarch64 means ARM, i.e. apple silicon
-        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-        modules = [ ./home.nix ];
-      };
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, homebrew-aerospace }: {
+
+    darwinConfigurations."Anhs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "anhbientuan";
+
+            # Automatically migrate existing Homebrew installations
+            autoMigrate = true;
+
+            # Optional: Declarative tap management
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+              "homebrew/homebrew-bundle" = homebrew-bundle;
+              "nikitabobko/homebrew-aerospace" = homebrew-aerospace;
+            };
+
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            mutableTaps = false;
+          };
+        }
+        ./configuration.nix
+      ];
+      specialArgs = { inherit inputs; };
     };
   };
 }
